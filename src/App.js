b3ef33a691c3d024e2cdca3a0b787b590b8e7cc8
victor <vemb96@gmail.com>
2018-11-19 13:22:49 -0300

    add material-ui

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {MuiThemeProvider} from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import {purple} from '@material-ui/core/colors';
+import Button from '@material-ui/core/Button';
 import './App.css';
 import Title from './title.js';
+
+  
 export default class App extends Component {
+
   render() {
     return (
-      <section>
-
+      <MuiThemeProvider >
         <div>
           <div>
-            <Title></Title>
-            <button>registrate gratis</button>
-           
             <div>
+              <Title></Title>
+              <Button variant="contained" color="secondary">registrate gratis</Button>
+              <img src={process.env.PUBLIC_URL + '/image/maptravel.png'} alt="viaje" height='300px'/>
+            </div> 
+            
+            <div style={{'backgroundColor':purple[400]}}>
               <ul>
                 <li>
                   <h3>calificaciones con emociones</h3>
@@ -33,7 +41,7 @@ export default class App extends Component {
           
         </div>
 
-      </section>
+      </MuiThemeProvider>
     );
   }
 }
--- src/title.js
+++ src/title.js
@@ -5,8 +5,8 @@ export default class Title extends React.Component{
   render(){
     return (
       <div>
-        <h1>places</h1>
-        <p> hola mundo </p>
+        <h1>findness</h1>
+        <p>encuentra tu lugar!!!</p>
       </div>
     );
   }
