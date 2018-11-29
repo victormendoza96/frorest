e9dbd7fe3ee19b1d4f6786687252072d5fdfc320
victor <vemb96@gmail.com>
2018-11-29 19:54:57 -0300

    integracion de card m-ui

--- src/App.js
+++ src/App.js
@@ -1,46 +1,96 @@
 import React, { Component } from 'react';
-import {MuiThemeProvider} from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import {purple} from '@material-ui/core/colors';
-import Button from '@material-ui/core/Button';
 import './App.css';
+import {MuiThemeProvider,createMuiTheme} from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+import {deepPurple} from '@material-ui/core/colors';
+import Button from '@material-ui/core/Button';
 import Title from './title.js';
 
-  
+const theme = createMuiTheme({
+  palette: {
+    primary:  {main: deepPurple[400]},
+  },
+   typography: {
+    useNextVariants: true,
+  },
+});
 export default class App extends Component {
 
   render() {
     return (
-      <MuiThemeProvider >
-        <div>
-          <div>
-            <div>
+      <MuiThemeProvider theme= {theme} >
+        <div className="center-body">
+          <div style={{"width":"80%","margin":"0 auto"}}>
+            <div className = "main-content">
               <Title></Title>
-              <Button variant="contained" color="secondary">registrate gratis</Button>
-              <img src={process.env.PUBLIC_URL + '/image/maptravel.png'} alt="viaje" height='300px'/>
+              <Button variant="contained" color="primary">registrate gratis</Button>
+              <img className="main-img" src={process.env.PUBLIC_URL + '/image/maptravel.png'} alt="viaje"/>
             </div> 
-            
-            <div style={{'backgroundColor':purple[400]}}>
+            <div>
               <ul>
-                <li>
-                  <h3>calificaciones con emociones</h3>
-                  <p>califica tus lugares con experiencias, no con numeros</p>
-                </li>
-                <li>
-                  <h3>¿Sin Internet? Sin Problemas</h3>
-                  <p>Findnes funciona sin internet y en conecciones lentas</p>
-                </li>
-                <li>
-                  <h3>Tus lugares favoritos</h3>
-                  <p>Define tu lista de sitios favoritos</p>
-                </li>
-              </ul>
-            
+                <Card className = "card-style">
+                  <div className="card-text" >
+                    <CardMedia className="img-property"
+                      style={{'backgroundColor':deepPurple[50]}}
+                      image='/image/heart.png'
+                      title="Live from space album cover"
+                    />
+                  <CardContent>
+                    <Typography component="h5" variant="h5">
+                      calificaciones con emociones
+                    </Typography>
+                    <Typography variant="subtitle1" color="textSecondary">
+                      califica tus lugares con experiencias, no con numeros
+                      </Typography>
+                  </CardContent>
+                  </div>
+                </Card>
+                
+                <Card className = "card-style">
+                  <div className="card-text" >
+                    <CardMedia className="img-property"
+                      style={{'backgroundColor':deepPurple[50]}}
+                      image='/image/star.png'
+                      title="Live from space album cover"
+                    />
+                    <CardContent>
+                      <Typography component="h5" variant="h5">
+                       calificaciones con emociones
+                      </Typography>
+                      <Typography variant="subtitle1" color="textSecondary">
+                       califica tus lugares con experiencias, no con numeros
+                      </Typography>
+                    </CardContent>
+                  </div>
+                </Card>
+                
+                <Card className = "card-style">
+                  <div className="card-text" >
+                    <CardMedia className="img-property"
+                      style={{'backgroundColor':deepPurple[50]}}
+                      image='/image/wifi.png'
+                      title="siempre conectado"
+                    />
+                    <CardContent>
+                      <Typography component="h5" variant="h5">
+                        sin wifi? sin problema!!
+                      </Typography>
+                      <Typography variant="subtitle1" color="textSecondary">
+                        findness funciona sin internet o en conexiones lentas
+                      </Typography>
+                    </CardContent>
+                  </div>
+                </Card>   
+              </ul>  
             </div>
           </div>
+        </div>
+        <div style={{'backgroundColor':deepPurple[400], 'padding': '50px'}}>
           
         </div>
-
       </MuiThemeProvider>
     );
   }
